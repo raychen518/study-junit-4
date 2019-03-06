0869ea57c244a63a9197fe228b328a1e771d2741
Nan Chen <nan.chen@concentrix.com>
2019-03-06 16:46:58 +0800

    Add the Getting Started test.

---
+++ src/main/java/org/raychen518/study/junit4/examples/gettingstarted/Adder.java
@@ -0,0 +1,9 @@
+package org.raychen518.study.junit4.examples.gettingstarted;
+
+public class Adder {
+
+	public int add(int x, int y) {
+		return (x + y);
+	}
+
+}
---
+++ src/test/java/org/raychen518/study/junit4/examples/gettingstarted/AdderTest.java
@@ -0,0 +1,18 @@
+package org.raychen518.study.junit4.examples.gettingstarted;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AdderTest {
+
+	@Test
+	public void testAdd() {
+		Adder adder = new Adder();
+
+		int result = adder.add(1, 2);
+
+		assertEquals(3, result);
+	}
+
+}
